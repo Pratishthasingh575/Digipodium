f6ad42605530289ba0a7d1216ca2458880edb023
Pratishtha <pratishthasingh575@gmail.com>
2023-06-26 23:06:06 +0530

    commit

---
+++ decagon.py
@@ -0,0 +1,27 @@
+from turtle import *
+
+speed ('slowest')
+#decagon
+fd(80)
+lt(36)
+fd(80)
+lt(36)
+fd(80)
+lt(36)
+fd(80)
+lt(36)
+fd(80)
+lt(36)
+fd(80)
+lt(36)
+fd(80)
+lt(36)
+fd(80)
+lt(36)
+fd(80)
+lt(36)
+fd(80)
+lt(36)
+
+hideturtle()
+mainloop()
---
+++ pentagon.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+speed('slowest')
+#pentagone
+fd(120)
+lt(72)
+fd(120)
+lt(72)
+fd(120)
+lt(72)
+fd(120)
+lt(72)
+fd(120)
+lt(72)
+
+hideturtle()
+mainloop()
